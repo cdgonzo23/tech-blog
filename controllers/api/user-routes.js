0ccf69bc6446b469e8ac2b04c2b2be69d5723217
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-07-27 13:39:53 -0700

    added login and logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,65 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// sign in
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
 
+        req.session.save(() => {
+            req.session.loggedIn = true;
 
+            res.status(200).json(dbUserData);
+        })
+    } catch (err) {
+        console.log(err);
+        res.status.json(err);
+    }
+});
+
+// Log In
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Incorrect username or password' })
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password' })
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({ user: dbUserData, message: 'Logged in successfully!' });
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,7 @@ const { Blogpost, User } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        const dbBlogpostData = await Blogpost.findAll({
-            attributes: ['title', 'author', 'createdOn', 'description']
-        });
+        const dbBlogpostData = await Blogpost.findAll();
         const blogposts = dbBlogpostData.map((blogpost) => 
             blogpost.get({ plain: true})
         );
@@ -17,6 +15,42 @@ router.get('/', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
+});
+
+router.get('/blogpost/:id', async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+      try {
+         const dbBlogpostData = await Blogpost.findByPk(req.params.id);
+         const blogpost = dbBlogpostData.get({ plain: true });
+         res.render('blogpost', { blogpost, loggedIn: req.session.loggedIn });
+      } catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+      }
+    }
 })
+
+// router.get('/dashboard', async (req, res) => {
+//     if (!req.session.loggedIn) {
+//         res.redirect('/login');
+//       } else {
+//         try {
+           
+//         } catch (err) {
+//             console.log(err);
+//             res.status(500).json(err);
+//         }
+//       }
+// });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    };
+    res.render('login');
+});
 
 module.exports = router;
