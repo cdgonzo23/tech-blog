4e8f7a87c7a450ac4364606b44234d4d08480a93
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-08-01 12:27:50 -0700

    added update and delete functionality, client side javascript

--- public/js/deleteBlogpost.js
+++ public/js/deleteBlogpost.js
@@ -1,6 +1,25 @@
 const deletePost = async (event) => {
     event.preventDefault();
     
+    const title = document.querySelector("#update-title").value;
+    console.log(title);
+    const description = document.querySelector("#update-description").value;
+    console.log(description);
+    const id = document.querySelector('#update-blogpost').getAttribute("post-id")
+    console.log(id)
+  
+    if (title && description) {
+        const response = await fetch(`/api/post/${id}`, {
+            method: "DELETE",
+            body: JSON.stringify({ title, description }),
+            headers: { "Content-Type": "application/json" },
+          });
+          if (response.ok) {
+            document.location.replace('/dashboard')    
+          }
+    } else {
+        alert('failed to delete post.');
+    }
   };
   
   document.querySelector("#delete-btn").addEventListener("click", deletePost);
--- public/js/updateBlogpost.js
+++ public/js/updateBlogpost.js
@@ -1,6 +1,26 @@
 const updatePost = async (event) => {
     event.preventDefault();
+
+    const title = document.querySelector("#update-title").value;
+    console.log(title);
+    const description = document.querySelector("#update-description").value;
+    console.log(description);
+    const id = document.querySelector('#update-blogpost').getAttribute("post-id")
+    console.log(id)
   
+    if (title && description) {
+        const response = await fetch(`/api/post/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({ title, description }),
+            headers: { "Content-Type": "application/json" },
+          });
+          if (response.ok) {
+            document.location.replace('/dashboard')    
+          }
+    } else {
+        alert('failed to update post.');
+    }
+
   };
   
 document.querySelector("#update-blogpost").addEventListener("submit", updatePost);
